a8d19c8b4a5be4f1e145e3041b0731d90c24bb3e
Edwin <edshen17@avenues.org>
2020-12-19 11:39:46 -0500

    update token

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const api = require('./routes/api/api');
 
 app.use('/api/', api);
 
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- server/routes/api/api.js
+++ server/routes/api/api.js
@@ -61,18 +61,11 @@ router.post('/register', (req, res) => {
 
 // route to get access to user's own information
 router.get('/me', VerifyToken, function(req, res, next) {
-  const token = req.headers['x-access-token'];
-  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-  
-  jwt.verify(token, config.secret, function(err, decoded) {
-    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-    
-  User.findById(decoded.id, { password: 0 }, function (err, user) {
+  User.findById(req.userId, { password: 0 }, function (err, user) {
     if (err) return res.status(500).send("There was a problem finding the user.");
     if (!user) return res.status(404).send("No user found.");
     
     next(user);
-  });
   });
 });
 
