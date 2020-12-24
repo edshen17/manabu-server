04a1b1e7ef1cfab6a3aee8199ee657a65a3681cb
Edwin <edshen17@avenues.org>
2020-12-24 11:46:49 -0500

    update error handling in verifytoken

--- server/routes/api/VerifyToken.js
+++ server/routes/api/VerifyToken.js
@@ -5,14 +5,11 @@ function verifyToken(req, res, next) {
   if ((req.headers['x-requested-with'] && req.cookies.hp && req.cookies.sig)) { //req contains the header (prevent csrf attacks) and is from client
     const token = req.cookies.hp + req.cookies.sig;
     jwt.verify(token, config.secret, async function(err, decoded) {
-      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      if (err) return next(err);
       // if everything good, save to request for use in other routes  
       req.userId = decoded.id;
       req.role = decoded.role;
     });
-
-  } else {
-    return res.status(403).send({ auth: false, message: 'No token provided.' });
   }
 
   next();
