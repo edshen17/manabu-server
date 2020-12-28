d41153e9cab1854df3167cd742211e1c440f5e5d
Edwin <edshen17@avenues.org>
2020-12-28 17:24:51 -0500

    update availabletime model

--- server/models/AvailableTime.js
+++ server/models/AvailableTime.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AvailableTimeSchema = new mongoose.Schema({
-  createdBy: { // user id
+  createdBy: { // user id (admin/teacher)
       type: Schema.Types.ObjectId, 
       required: true 
+    },
+    reservedBy: { // user id (student/teacher)
+      type: Schema.Types.ObjectId, 
+      default: '', 
+    },
+    packageId: { // package id
+      type: Schema.Types.ObjectId, 
+      default: '', 
     },
   from: { // iso format of date (start time)
     type: String,
@@ -12,8 +20,12 @@ const AvailableTimeSchema = new mongoose.Schema({
   },
   to: { // (end time)
       type: String,
-      default: true,
+      required: true,
   },
+  status: { // status of appointment (completed, reserved, pending, cancelled)
+    type: String,
+    default: 'pending',
+},
 });
 
 
