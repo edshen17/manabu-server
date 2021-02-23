dff67143ddd5f45d119f9aca35c596690d03d812
edshen17 <shen.ed@husky.neu.edu>
2021-02-23 11:58:39 -0500

    deployment prep

--- server/index.js
+++ server/index.js
@@ -24,6 +24,14 @@ const api = require('./routes/api/api');
 app.use('/api/', api);
 app.set('trust proxy', 1)
 
+if (process.env.NODE_ENV == 'production') {
+  // static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // handle spa
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
