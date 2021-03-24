75832f0835151a93091dc8b17f3716c457ff6bc8
edshen17 <shen.ed@husky.neu.edu>
2021-03-23 23:28:15 -0400

    undo

--- server/scripts/controller/errorHandler.js
+++ server/scripts/controller/errorHandler.js
@@ -1,7 +1,25 @@
 const { GeneralError } = require('./utils/errors');
+const dotenv = require('dotenv').config();
 
 const handleErrors = (err, req, res, next) => {
-  console.log(err)
+  let transporter = nodemailer.createTransport({
+    host: 'mail.privateemail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'support@manabu.sg',
+      pass: process.env.MANABU_EMAIL_SUPPORT_PASS,
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'Manabu Support <support@manabu.sg>', // sender address
+    to: 'greencopter4444@gmail.com', // list of receivers
+    subject: "err", // Subject line
+    html: `${err}`,
+  }).catch((err) => { console.log(err) });
+
   if (err instanceof GeneralError) {
     return res.status(err.getCode()).json({
       status: 'error',
