b09260aea19c294dbc017e981ee71cce016bc4cb
edshen17 <shen.ed@husky.neu.edu>
2021-05-24 21:32:18 -0400

    update getUserUsecase

--- server/components/usecases/user/getUserUsecase.js
+++ server/components/usecases/user/getUserUsecase.js
@@ -1,10 +1,11 @@
 function makeGetUserUsecase({ usersDb }) {
   return async function getUser({ uId, currentAPIUser, path } = {}) {
-    if (uId || currentAPIUser.userId) {
-      const idToSearch = path == '/me' ? currentAPIUser.userId : uId;
-      const user = await usersDb.findById(idToSearch, currentAPIUser);
-      return user;
+    if (!uId && !currentAPIUser.userId) {
+      throw new Error();
     }
+    const idToSearch = path == '/me' ? currentAPIUser.userId : uId;
+    const user = await usersDb.findById(idToSearch, currentAPIUser);
+    return user;
   };
 }
 module.exports = { makeGetUserUsecase };
