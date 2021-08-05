e95fa922e15a059e6513728c4f3442f6cdfa95ec
edshen17 <shen.ed@husky.neu.edu>
2021-08-05 10:20:13 -0400

    finish createAvailableTime route

--- server/routes/api/v1/availableTimes/index.ts
+++ server/routes/api/v1/availableTimes/index.ts
@@ -1,17 +1,9 @@
 import express from 'express';
-import { makeCreateUserController } from '../../../../components/controllers/user/createUserController';
-import { makeEditUserController } from '../../../../components/controllers/user/editUserController';
-import { makeGetUserController } from '../../../../components/controllers/user/getUserController';
-import {
-  makeJSONCookieExpressCallback,
-  makeJSONExpressCallback,
-} from '../../../../components/webFrameworkCallbacks/callbacks/expressCallback';
-const users = express.Router();
+import { makeCreateAvailableTimeController } from '../../../../components/controllers/availableTime/createAvailableTimeController';
 
-users.get('/:userId', makeJSONExpressCallback.consume(makeGetUserController));
-users.patch('/:userId', makeJSONExpressCallback.consume(makeEditUserController));
+import { makeJSONExpressCallback } from '../../../../components/webFrameworkCallbacks/callbacks/expressCallback';
+const availableTime = express.Router();
 
-users.get('/self', makeJSONExpressCallback.consume(makeGetUserController));
-users.post('/create', makeJSONCookieExpressCallback.consume(makeCreateUserController));
+availableTime.post('/create', makeJSONExpressCallback.consume(makeCreateAvailableTimeController));
 
-export { users };
+export { availableTime };
