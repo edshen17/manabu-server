e0c4356ed65e72b0c5bb99e3ec3efdd314406906
Edwin <edshen17@avenues.org>
2022-02-16 11:10:01 -0500

    fix paypal issue

--- server/components/dataAccess/services/json/jsonDbService.ts
+++ server/components/dataAccess/services/json/jsonDbService.ts
@@ -1,23 +1,23 @@
-class JsonDbService {
-  private _client!: any;
-  private _entity!: any;
-  private _schema!: any;
-  private _repository!: any;
+// class JsonDbService {
+//   private _client!: any;
+//   private _entity!: any;
+//   private _schema!: any;
+//   private _repository!: any;
 
-  public init = async (initParams: {
-    client: any;
-    entity: any;
-    schema: any;
-    repository: any;
-  }): Promise<this> => {
-    const { client, entity, schema, repository } = initParams;
-    this._client = client;
-    this._entity = entity;
-    this._schema = schema;
-    this._repository = repository;
-    await this._client.open(redisUrl);
-    return this;
-  };
-}
+//   public init = async (initParams: {
+//     client: any;
+//     entity: any;
+//     schema: any;
+//     repository: any;
+//   }): Promise<this> => {
+//     const { client, entity, schema, repository } = initParams;
+//     this._client = client;
+//     this._entity = entity;
+//     this._schema = schema;
+//     this._repository = repository;
+//     await this._client.open(redisUrl);
+//     return this;
+//   };
+// }
 
-export { JsonDbService };
+// export { JsonDbService };
