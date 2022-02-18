304c066ef4a6499860e5ebbb61d5d42478fc0ce4
Edwin <edshen17@avenues.org>
2022-02-18 11:13:33 -0500

    refactor sendAppointmentReminderScheduleTask

--- server/components/usecases/utils/emailHandler/components/Logo.ts
+++ server/components/usecases/utils/emailHandler/components/Logo.ts
@@ -2,7 +2,7 @@ const Logo = {
   template: `
     <mj-section>
       <mj-column padding="10px" width="100%">
-        <mj-image src="https://storage.googleapis.com/japanese-221819.appspot.com/cdn/toori-sm.png" width="65px" alt="logo" />
+        <mj-image src="https://storage.googleapis.com/japanese-221819.appspot.com/cdn/toori-sm.png" width="70px" height="80px" alt="logo" />
       </mj-column>
     </mj-section>
   `,
