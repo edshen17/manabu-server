b9eaca9fa444dbcfdb4aa408d1fdb248ad1ad1ad
Edwin Shen <edshen17@avenues.org>
2022-03-17 14:31:17 -0400

    finish contentDbService

--- server/components/dataAccess/services/content/contentDbService.test.ts
+++ server/components/dataAccess/services/content/contentDbService.test.ts
@@ -1,252 +1,252 @@
-// import { expect } from 'chai';
-// import { makeAvailableTimeDbService } from '.';
-// import { AvailableTimeDoc } from '../../../../models/AvailableTime';
-// import { DbServiceAccessOptions } from '../../abstractions/IDbService';
-// import { makeFakeDbAvailableTimeFactory } from '../../testFixtures/fakeDbAvailableTimeFactory';
-// import { FakeDbAvailableTimeFactory } from '../../testFixtures/fakeDbAvailableTimeFactory/fakeDbAvailableTimeFactory';
-// import { ContentDbService } from './contentDbService';
+import { expect } from 'chai';
+import { makeContentDbService } from '.';
+import { ContentDoc } from '../../../../models/Content';
+import { DbServiceAccessOptions } from '../../abstractions/IDbService';
+import { makeFakeDbContentFactory } from '../../testFixtures/fakeDbContentFactory';
+import { FakeDbContentFactory } from '../../testFixtures/fakeDbContentFactory/fakeDbContentFactory';
+import { ContentDbService } from './contentDbService';
 
-// let contentDbService: ContentDbService;
-// let fakeDbAvailableTimeFactory: FakeDbAvailableTimeFactory;
-// let dbServiceAccessOptions: DbServiceAccessOptions;
-// let fakeAvailableTime: AvailableTimeDoc;
+let contentDbService: ContentDbService;
+let fakeDbContentFactory: FakeDbContentFactory;
+let dbServiceAccessOptions: DbServiceAccessOptions;
+let fakeContent: ContentDoc;
 
-// before(async () => {
-//   contentDbService = await makeAvailableTimeDbService;
-//   fakeDbAvailableTimeFactory = await makeFakeDbAvailableTimeFactory;
-// });
+before(async () => {
+  contentDbService = await makeContentDbService;
+  fakeDbContentFactory = await makeFakeDbContentFactory;
+});
 
-// beforeEach(async () => {
-//   dbServiceAccessOptions = contentDbService.getBaseDbServiceAccessOptions();
-//   fakeAvailableTime = await fakeDbAvailableTimeFactory.createFakeDbData();
-// });
+beforeEach(async () => {
+  dbServiceAccessOptions = contentDbService.getBaseDbServiceAccessOptions();
+  fakeContent = await fakeDbContentFactory.createFakeDbData();
+});
 
-// describe('contentDbService', () => {
-//   describe('findById, findOne, find', () => {
-//     context('db access permitted', () => {
-//       context('invalid inputs', () => {
-//         it('should throw an error if given an invalid id', async () => {
-//           try {
-//             await contentDbService.findById({
-//               _id: undefined,
-//               dbServiceAccessOptions,
-//             });
-//           } catch (err) {
-//             expect(err).be.an('error');
-//           }
-//         });
-//         it('should return null if given an non-existent id', async () => {
-//           const findByIdAvailableTime = await contentDbService.findById({
-//             _id: '60979db0bb31ed001589a1ea',
-//             dbServiceAccessOptions,
-//           });
-//           expect(findByIdAvailableTime).to.equal(null);
-//         });
-//       });
-//       context('valid inputs', () => {
-//         const getAvailableTime = async () => {
-//           const findParams = {
-//             searchQuery: {
-//               hostedById: fakeAvailableTime.hostedById,
-//             },
-//             dbServiceAccessOptions,
-//           };
-//           const findByIdAvailableTime = await contentDbService.findById({
-//             _id: fakeAvailableTime._id,
-//             dbServiceAccessOptions,
-//           });
-//           const findOneAvailableTime = await contentDbService.findOne(findParams);
-//           const findAvailableTimes = await contentDbService.find(findParams);
-//           expect(findByIdAvailableTime).to.deep.equal(findOneAvailableTime);
-//           expect(findByIdAvailableTime).to.deep.equal(findAvailableTimes[0]);
-//         };
-//         context('as a non-admin user', () => {
-//           context('viewing self', () => {
-//             it('should find the AvailableTime and return an unrestricted view on some data', async () => {
-//               dbServiceAccessOptions.isSelf = true;
-//               await getAvailableTime();
-//             });
-//           });
-//           context('viewing other', () => {
-//             it('should find the AvailableTime and return an unrestricted view on some data', async () => {
-//               await getAvailableTime();
-//             });
-//           });
-//         });
-//         context('as an admin', () => {
-//           it('should find the AvailableTime and return an restricted view on some data', async () => {
-//             dbServiceAccessOptions.currentAPIUserRole = 'admin';
-//             await getAvailableTime();
-//           });
-//         });
-//       });
-//     });
-//     context('db access denied', () => {
-//       it('should throw an error', async () => {
-//         dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
-//         let err;
-//         try {
-//           err = await contentDbService.findById({
-//             _id: fakeAvailableTime._id,
-//             dbServiceAccessOptions,
-//           });
-//         } catch (err) {
-//           return;
-//         }
-//         expect(err).to.be.an('error');
-//       });
-//     });
-//   });
-//   describe('insert', () => {
-//     context('db access permitted', () => {
-//       context('invalid inputs', () => {
-//         it('should throw an error if required fields are not given', async () => {
-//           try {
-//             fakeAvailableTime = await contentDbService.insert({
-//               modelToInsert: {},
-//               dbServiceAccessOptions,
-//             });
-//           } catch (err) {
-//             expect(err).to.be.an('error');
-//           }
-//         });
-//       });
-//       context('valid inputs', () => {
-//         it('should insert an AvailableTime', async () => {
-//           const findByIdAvailableTime = await contentDbService.findById({
-//             _id: fakeAvailableTime._id,
-//             dbServiceAccessOptions,
-//           });
-//           expect(findByIdAvailableTime).to.not.equal(null);
-//           expect(findByIdAvailableTime).to.deep.equal(fakeAvailableTime);
-//         });
-//       });
-//     });
-//     context('db access denied', () => {
-//       it('should throw an error', async () => {
-//         dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
-//         const { _id, ...modelToInsert } = fakeAvailableTime;
-//         try {
-//           fakeAvailableTime = await contentDbService.insert({
-//             modelToInsert,
-//             dbServiceAccessOptions,
-//           });
-//         } catch (err: any) {
-//           expect(err.message).to.equal('Access denied.');
-//         }
-//       });
-//     });
-//   });
-//   describe('update', () => {
-//     const updateAvailableTime = async () => {
-//       const updatedAvailableTime = await contentDbService.findOneAndUpdate({
-//         searchQuery: { _id: fakeAvailableTime._id },
-//         updateQuery: { startDate: new Date() },
-//         dbServiceAccessOptions,
-//       });
-//       expect(updatedAvailableTime).to.not.deep.equal(fakeAvailableTime);
-//       expect(updatedAvailableTime.startDate).to.not.equal(fakeAvailableTime.startDate);
-//     };
-//     context('db access permitted', () => {
-//       context('invalid inputs', () => {
-//         it('should return null if the availableTime to update does not exist', async () => {
-//           const updatedAvailableTime = await contentDbService.findOneAndUpdate({
-//             searchQuery: {
-//               _id: fakeAvailableTime.hostedById,
-//             },
-//             updateQuery: { status: 'cancelled' },
-//             dbServiceAccessOptions,
-//           });
-//           expect(updatedAvailableTime).to.equal(null);
-//         });
-//       });
-//       context('valid inputs', () => {
-//         context('as a non-admin user', () => {
-//           context('updating self', () => {
-//             it('should update the availableTime', async () => {
-//               dbServiceAccessOptions.isSelf = true;
-//               await updateAvailableTime();
-//             });
-//           });
-//           context('updating other', async () => {
-//             it('should update the availableTime', async () => {
-//               await updateAvailableTime();
-//             });
-//           });
-//         });
-//         context('as an admin', async () => {
-//           it('should update the availableTime', async () => {
-//             dbServiceAccessOptions.currentAPIUserRole = 'admin';
-//             await updateAvailableTime();
-//           });
-//         });
-//       });
-//     });
-//     context('db access denied', () => {
-//       it('should throw an error', async () => {
-//         dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
-//         try {
-//           await updateAvailableTime();
-//         } catch (err: any) {
-//           expect(err.message).to.equal('Access denied.');
-//         }
-//       });
-//     });
-//   });
-//   describe('delete', () => {
-//     const deleteAvailableTime = async () => {
-//       const deletedAvailableTime = await contentDbService.findByIdAndDelete({
-//         _id: fakeAvailableTime._id,
-//         dbServiceAccessOptions,
-//       });
-//       const foundAvailableTime = await contentDbService.findById({
-//         _id: fakeAvailableTime._id,
-//         dbServiceAccessOptions,
-//       });
-//       expect(foundAvailableTime).to.not.deep.equal(deletedAvailableTime);
-//       expect(foundAvailableTime).to.be.equal(null);
-//     };
-//     context('db access permitted', () => {
-//       context('invalid inputs', () => {
-//         it('should return null if the availableTime to delete does not exist', async () => {
-//           const deletedAvailableTime = await contentDbService.findByIdAndDelete({
-//             _id: fakeAvailableTime.hostedById,
-//             dbServiceAccessOptions,
-//           });
-//           expect(deletedAvailableTime).to.equal(null);
-//         });
-//       });
-//       context('valid inputs', () => {
-//         context('as a non-admin user', () => {
-//           context('deleting self', () => {
-//             it('should update the availableTime', async () => {
-//               dbServiceAccessOptions.isSelf = true;
-//               await deleteAvailableTime();
-//             });
-//           });
-//           context('deleting other', async () => {
-//             it('should update the availableTime', async () => {
-//               await deleteAvailableTime();
-//             });
-//           });
-//         });
-//         context('as an admin', async () => {
-//           it('should update the availableTime', async () => {
-//             dbServiceAccessOptions.currentAPIUserRole = 'admin';
-//             await deleteAvailableTime();
-//           });
-//         });
-//       });
-//     });
-//     context('db access denied', () => {
-//       it('should throw an error', async () => {
-//         dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
-//         try {
-//           await deleteAvailableTime();
-//         } catch (err: any) {
-//           expect(err.message).to.equal('Access denied.');
-//         }
-//       });
-//     });
-//   });
-// });
+describe('contentDbService', () => {
+  describe('findById, findOne, find', () => {
+    context('db access permitted', () => {
+      context('invalid inputs', () => {
+        it('should throw an error if given an invalid id', async () => {
+          try {
+            await contentDbService.findById({
+              _id: undefined,
+              dbServiceAccessOptions,
+            });
+          } catch (err) {
+            expect(err).be.an('error');
+          }
+        });
+        it('should return null if given an non-existent id', async () => {
+          const findByIdContent = await contentDbService.findById({
+            _id: '60979db0bb31ed001589a1ea',
+            dbServiceAccessOptions,
+          });
+          expect(findByIdContent).to.equal(null);
+        });
+      });
+      context('valid inputs', () => {
+        const getContent = async () => {
+          const findParams = {
+            searchQuery: {
+              _id: fakeContent._id,
+            },
+            dbServiceAccessOptions,
+          };
+          const findByIdContent = await contentDbService.findById({
+            _id: fakeContent._id,
+            dbServiceAccessOptions,
+          });
+          const findOneContent = await contentDbService.findOne(findParams);
+          const findContent = await contentDbService.find(findParams);
+          expect(findByIdContent).to.deep.equal(findOneContent);
+          expect(findByIdContent).to.deep.equal(findContent[0]);
+        };
+        context('as a non-admin user', () => {
+          context('viewing self', () => {
+            it('should find the content and return an unrestricted view on some data', async () => {
+              dbServiceAccessOptions.isSelf = true;
+              await getContent();
+            });
+          });
+          context('viewing other', () => {
+            it('should find the content and return an unrestricted view on some data', async () => {
+              await getContent();
+            });
+          });
+        });
+        context('as an admin', () => {
+          it('should find the content and return an restricted view on some data', async () => {
+            dbServiceAccessOptions.currentAPIUserRole = 'admin';
+            await getContent();
+          });
+        });
+      });
+    });
+    context('db access denied', () => {
+      it('should throw an error', async () => {
+        dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
+        let err;
+        try {
+          err = await contentDbService.findById({
+            _id: fakeContent._id,
+            dbServiceAccessOptions,
+          });
+        } catch (err) {
+          return;
+        }
+        expect(err).to.be.an('error');
+      });
+    });
+  });
+  describe('insert', () => {
+    context('db access permitted', () => {
+      context('invalid inputs', () => {
+        it('should throw an error if required fields are not given', async () => {
+          try {
+            fakeContent = await contentDbService.insert({
+              modelToInsert: {},
+              dbServiceAccessOptions,
+            });
+          } catch (err) {
+            expect(err).to.be.an('error');
+          }
+        });
+      });
+      context('valid inputs', () => {
+        it('should insert an Content', async () => {
+          const findByIdContent = await contentDbService.findById({
+            _id: fakeContent._id,
+            dbServiceAccessOptions,
+          });
+          expect(findByIdContent).to.not.equal(null);
+          expect(findByIdContent).to.deep.equal(fakeContent);
+        });
+      });
+    });
+    context('db access denied', () => {
+      it('should throw an error', async () => {
+        dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
+        const { _id, ...modelToInsert } = fakeContent;
+        try {
+          fakeContent = await contentDbService.insert({
+            modelToInsert,
+            dbServiceAccessOptions,
+          });
+        } catch (err: any) {
+          expect(err.message).to.equal('Access denied.');
+        }
+      });
+    });
+  });
+  describe('update', () => {
+    const updateContent = async () => {
+      const updatedContent = await contentDbService.findOneAndUpdate({
+        searchQuery: { _id: fakeContent._id },
+        updateQuery: { title: 'new title' },
+        dbServiceAccessOptions,
+      });
+      expect(updatedContent).to.not.deep.equal(fakeContent);
+      expect(updatedContent.title).to.not.equal(fakeContent.title);
+    };
+    context('db access permitted', () => {
+      context('invalid inputs', () => {
+        it('should return null if the content to update does not exist', async () => {
+          const updatedAvailableTime = await contentDbService.findOneAndUpdate({
+            searchQuery: {
+              _id: '605bc5ad9db900001528f77c',
+            },
+            updateQuery: { status: 'cancelled' },
+            dbServiceAccessOptions,
+          });
+          expect(updatedAvailableTime).to.equal(null);
+        });
+      });
+      context('valid inputs', () => {
+        context('as a non-admin user', () => {
+          context('updating self', () => {
+            it('should update the content', async () => {
+              dbServiceAccessOptions.isSelf = true;
+              await updateContent();
+            });
+          });
+          context('updating other', async () => {
+            it('should update the content', async () => {
+              await updateContent();
+            });
+          });
+        });
+        context('as an admin', async () => {
+          it('should update the content', async () => {
+            dbServiceAccessOptions.currentAPIUserRole = 'admin';
+            await updateContent();
+          });
+        });
+      });
+    });
+    context('db access denied', () => {
+      it('should throw an error', async () => {
+        dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
+        try {
+          await updateContent();
+        } catch (err: any) {
+          expect(err.message).to.equal('Access denied.');
+        }
+      });
+    });
+  });
+  describe('delete', () => {
+    const deleteContent = async () => {
+      const deletedAvailableTime = await contentDbService.findByIdAndDelete({
+        _id: fakeContent._id,
+        dbServiceAccessOptions,
+      });
+      const foundAvailableTime = await contentDbService.findById({
+        _id: fakeContent._id,
+        dbServiceAccessOptions,
+      });
+      expect(foundAvailableTime).to.not.deep.equal(deletedAvailableTime);
+      expect(foundAvailableTime).to.be.equal(null);
+    };
+    context('db access permitted', () => {
+      context('invalid inputs', () => {
+        it('should return null if the content to delete does not exist', async () => {
+          const deletedAvailableTime = await contentDbService.findByIdAndDelete({
+            _id: '605bc5ad9db900001528f77c',
+            dbServiceAccessOptions,
+          });
+          expect(deletedAvailableTime).to.equal(null);
+        });
+      });
+      context('valid inputs', () => {
+        context('as a non-admin user', () => {
+          context('deleting self', () => {
+            it('should update the content', async () => {
+              dbServiceAccessOptions.isSelf = true;
+              await deleteContent();
+            });
+          });
+          context('deleting other', async () => {
+            it('should update the content', async () => {
+              await deleteContent();
+            });
+          });
+        });
+        context('as an admin', async () => {
+          it('should update the content', async () => {
+            dbServiceAccessOptions.currentAPIUserRole = 'admin';
+            await deleteContent();
+          });
+        });
+      });
+    });
+    context('db access denied', () => {
+      it('should throw an error', async () => {
+        dbServiceAccessOptions.isCurrentAPIUserPermitted = false;
+        try {
+          await deleteContent();
+        } catch (err: any) {
+          expect(err.message).to.equal('Access denied.');
+        }
+      });
+    });
+  });
+});
--- server/components/dataAccess/services/content/contentDbService.ts
+++ server/components/dataAccess/services/content/contentDbService.ts
@@ -8,6 +8,21 @@ type ContentDbServiceResponse = ContentDoc;
 class ContentDbService extends AbstractDbService<
   OptionalContentDbServiceInitParams,
   ContentDbServiceResponse
-> {}
+> {
+  protected _getDbServiceModelViews = () => {
+    return {
+      defaultView: {
+        titleNGrams: 0,
+      },
+      adminView: {
+        titleNGrams: 0,
+      },
+      selfView: {
+        titleNGrams: 0,
+      },
+      overrideView: {},
+    };
+  };
+}
 
 export { ContentDbService, ContentDbServiceResponse };
