324ff061ec8e1d625a166c0df3377b1976b22efe
Edwin Shen <edshen17@avenues.org>
2022-04-03 18:14:17 -0400

    fix payment

--- server/components/payment/services/stripe/stripePaymentService.ts
+++ server/components/payment/services/stripe/stripePaymentService.ts
@@ -20,15 +20,14 @@ class StripePaymentService extends AbstractPaymentService<
     const { successRedirectUrl, cancelRedirectUrl, items, token } = props;
     const createPaymentJson = {
       line_items: items,
-      // payment_method_types: ['card', 'wechat_pay', 'grabpay', 'alipay'],
-      payment_method_types: ['card'],
+      payment_method_types: ['card', 'wechat_pay', 'grabpay', 'alipay'],
       mode: 'payment',
       success_url: successRedirectUrl,
       cancel_url: cancelRedirectUrl,
       payment_method_options: {
-        // wechat_pay: {
-        //   client: 'web',
-        // },
+        wechat_pay: {
+          client: 'web',
+        },
       },
       payment_intent_data: {
         metadata: { token },
